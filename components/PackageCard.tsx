b0bd41387ed67abfe52481b7ca0fa57e2c4241ef
lochana2002 <ehltehelapitiya@gmail.com>
2026-04-04 15:19:20 +0530

    feat:add a button to cards

--- components/PackageCard.tsx
+++ components/PackageCard.tsx
@@ -8,18 +8,30 @@ export default function PackageCard({
   slug,
 }: any) {
   return (
-    <Link href={`/safari-packages/${slug}`}>
-      <div className="border rounded-xl overflow-hidden hover:shadow-lg">
+    <div className="border rounded-xl overflow-hidden hover:shadow-lg">
 
-        <img src={image} className="h-60 w-full object-cover" />
+      {/* Card Image */}
+      <img src={image} className="h-60 w-full object-cover" />
 
-        <div className="p-4">
-          <h3 className="font-bold text-xl text-gray-800 ">{title}</h3>
-          <p className="text-gray-500 ">{description}</p>
-          <p className="text-green-700">{price}</p>
-        </div>
+      {/* Content */}
+      <div className="p-4 text-center space-y-2">
+        <h3 className="font-bold text-xl text-gray-800">{title}</h3>
+        <p className="text-gray-500">{description}</p>
+        <p className="text-green-700 font-semibold">{price}</p>
 
+       <Link
+  href={`/safari-packages/${slug}`}
+  className="mt-4 flex justify-center"
+>
+  <span
+    className="px-6 py-3 bg-green-900 hover:bg-green-700
+               text-white font-semibold rounded-full shadow-md transition"
+  >
+    View Package 
+  </span>
+</Link>
       </div>
-    </Link>
+
+    </div>
   );
 }
